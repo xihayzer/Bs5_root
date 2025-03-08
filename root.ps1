090c9ae393cf608225ad14151f4f4704bd38cc4b
xihayzer <73201316+xihayzer@users.noreply.github.com>
2025-03-07 23:45:08 -0300

    Update root.ps1

--- root.ps1
+++ root.ps1
@@ -1,6 +1,6 @@
 # Define the paths
-$BlueStacksHome = (Get-ItemProperty "HKLM:\SOFTWARE\BlueStacks_nxt").UserDefinedDir
-$BlueStacksInstallDir = (Get-ItemProperty "HKLM:\SOFTWARE\BlueStacks_nxt").InstallDir
+$BlueStacksHome = (Get-ItemProperty "HKLM:\SOFTWARE\BlueStacks_msi5").UserDefinedDir
+$BlueStacksInstallDir = (Get-ItemProperty "HKLM:\SOFTWARE\BlueStacks_msi5").InstallDir
 $DesktopPath = [Environment]::GetFolderPath('Desktop')
 $BlueStacksConfig = Join-Path $BlueStacksHome "bluestacks.conf"
 $BlueStacksEngine = Join-Path $BlueStacksHome "Engine"
